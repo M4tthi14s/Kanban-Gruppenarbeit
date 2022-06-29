0ded6a2814882391fb8fe0cdb4c2b956e428cc06
M4tthi14s <Matthias.Speth@outlook.com>
2022-06-29 12:06:28 +0200

    board

--- js/board.js
+++ js/board.js
@@ -1,3 +1,41 @@
+function renderToDo(){
+    let allTasksArray = document.getElementById('note')
+
+    for (let i = 0; i < allTask.length; i++) {
+       if (allTasksArray[i].backlog == false){
+        column.innerHTML =
+        `
+        <div class="note" id="task_${i}"
+            <div>
+                <h2>${allTasksArray[i].names.replace('_', ' ')}</h2>
+                <h2>${allTasksArray[i].title}</h2>
+            </div>
+        </div>
+        <h2 id="task_${i}">${allTasksArray[i].category}</h2>
+        <p>${allTasksArray[i].description}</p>
+
+
+        `
+
+
+
+       }
+       
+       }
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 let currentDraggedElement;
 
 
