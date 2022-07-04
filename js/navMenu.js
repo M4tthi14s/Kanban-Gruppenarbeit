592735749a9201d0c1f786f6aea7cd2425a6b9ea
M4tthi14s <Matthias.Speth@outlook.com>
2022-07-04 14:11:50 +0200

    clean-code

--- js/navMenu.js
+++ js/navMenu.js
@@ -1,13 +1,9 @@
 function menuLink(index) {
-
     let menuLinkArray = ['headlineBody', 'board', 'backlog', 'addTask', 'howTo', 'copyright', "imprint", "privacy"];
     menuLinkArray.splice(menuLinkArray.indexOf(index), 1);
-
     showLink(index);
     hideLink(menuLinkArray);
 }
-
-
 function showLink(index) {
     document.getElementById(index).style.display = 'flex';
     // if (index == 'board' || index == 'backlog') { document.getElementById('hideSearchTitle').style.display = 'flex'; }
@@ -15,53 +11,37 @@ function showLink(index) {
     if (index == 'board') { document.getElementById('paperBin').style.display = 'flex'; }
     else { document.getElementById('paperBin').style.display = 'none'; }
 }
-
-
 function hideLink(menuLinkArray) {
     menuLinkArray.forEach(element => { document.getElementById(element).style.display = 'none'; });
 }
-
-
 function navBar() {
     document.getElementById('regulations').classList.toggle('d-none');
     document.getElementById('menuSelection').classList.toggle('d-none');
     // document.getElementById('hideSearchTitle').classList.toggle('d-none');
     document.getElementById('hidePaperBin').classList.toggle('d-none');
 }
-
-
 function paperBin() {
     document.getElementById('boardContainer').classList.toggle('d-none');
     document.getElementById('trashContainer').classList.toggle('d-none');
 }
-
-
 function searchTitle() {
     let search = document.getElementById('searchTitle').value;
     search = search.toLowerCase();
-
     let list = document.getElementById('outputSearch');
     list.innerHTML = '';
-
-    for (let i = 1; i < allTasksArray.length; i++) {
-        
-        const currentItem = allTasksArray[i].title.toLowerCase();
-        
+    for (let i = 1; i < allTasksArray.length; i++) {        
+        const currentItem = allTasksArray[i].title.toLowerCase();        
         if (currentItem.toLowerCase().includes(search) && search.length > 0) {
             list.innerHTML +=
                 `
                 <ul><a onclick="closeSearch()" href="#jump${i}">${currentItem}</a></ul>
                 `;
                 console.log(currentItem)
-        }
-      
-
+        }      
         if (search.length < 0) {
             document.getElementById('outputSearch').innerHTML = '';
             document.getElementById('searchInput').value = '';
         }
-
-
     };
 }
 
