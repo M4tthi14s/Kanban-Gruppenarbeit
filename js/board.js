ac37462554f6333114d577e994823261ae358aa4
M4tthi14s <Matthias.Speth@outlook.com>
2022-07-04 14:10:07 +0200

    clean-code

--- js/board.js
+++ js/board.js
@@ -21,51 +21,37 @@ function generateTodoHTML(element) {
             `
 }
 function renderAvatarPicFalse(index) {
-    // console.log(index)
     let num
     for (let i = 0; i < allTasksArray.length; i++) {
         if (allTasksArray[i].id == index) {
             num = i;
         }
     }
-    // if (allTasksArray[i].backlog == false) {
-    // console.log(num)
     for (let j = 0; j < allTasksArray[num].names.length; j++) {
-
         document.getElementById('backlogAvatar_' + index).innerHTML +=
             `<div class="avatarPicTitleBord">
                     <img class="backlogImg" src="./img/${allTasksArray[num].names[j]}.png">
                     <h2>${letterName(num, j)}</h2>
                 </div>`;
     }
-    // }
 }
-// letterName(i,j)
-// allTasksArray[num].names[j].replace('_', ' ')
 function renderPaperBin(element, dell) {
     if (dell == false) {
         return `<img onclick="deleteNote(${element['id']})" src="./img/close.svg"></img>`;
-        // return `<button onclick="deleteNote(${element['id']})">Dell</button>`;
     } else {
         return `<img onclick="paperBinNote(${element['id']})" src="./img/paperBin.png"></img>`;
-        // return `<button onclick="paperBinNote(${element['id']})">Paperbin</button>`;
     }
 }
-// async function renderAfterBin(){
-//  await downloadFromServer
-// } 
 function updateHTML() {
     let ifFalse = allTasksArray.filter(t => t['backlog'] == false);
     let toDo = ifFalse.filter(t => t['board'] == 'toDo');
     document.getElementById('toDo').innerHTML = '';
-
     for (let index = 0; index < toDo.length; index++) {
         const element = toDo[index];
         document.getElementById('toDo').innerHTML += generateTodoHTML(element);
         renderAvatarPicFalse(element['id']);
         document.getElementById('paperBin_' + element['id']).innerHTML = renderPaperBin(element);
     }
-
     let inProgress = ifFalse.filter(t => t['board'] == 'inProgress');
     document.getElementById('inProgress').innerHTML = '';
     for (let index = 0; index < inProgress.length; index++) {
@@ -74,7 +60,6 @@ function updateHTML() {
         renderAvatarPicFalse(element['id']);
         document.getElementById('paperBin_' + element['id']).innerHTML = renderPaperBin(element);
     }
-
     let testing = ifFalse.filter(t => t['board'] == 'testing');
     document.getElementById('testing').innerHTML = '';
     for (let index = 0; index < testing.length; index++) {
@@ -83,7 +68,6 @@ function updateHTML() {
         renderAvatarPicFalse(element['id']);
         document.getElementById('paperBin_' + element['id']).innerHTML = renderPaperBin(element);
     }
-
     let done = ifFalse.filter(t => t['board'] == 'done');
     document.getElementById('done').innerHTML = '';
     for (let index = 0; index < done.length; index++) {
@@ -92,7 +76,6 @@ function updateHTML() {
         renderAvatarPicFalse(element['id']);
         document.getElementById('paperBin_' + element['id']).innerHTML = renderPaperBin(element);
     }
-
     let trash = ifFalse.filter(t => t['board'] == 'trash');
     document.getElementById('trash').innerHTML = '';
     for (let index = 0; index < trash.length; index++) {
@@ -109,20 +92,17 @@ let currentDraggedElement;
 function startDragging(id) {
     currentDraggedElement = id;
 }
-
 function allowDrop(ev) {
     ev.preventDefault();
 }
 function moveTo(board) {
     for (let i = 0; i < allTasksArray.length; i++) {
         const content = allTasksArray[i].id;
-       
         if (content == currentDraggedElement)
-        allTasksArray[i]['board'] = board;
+            allTasksArray[i]['board'] = board;
         pushToServer();
         updateHTML();
     }
- 
 }
 function highlight(id) {
     document.getElementById(id).classList.add('box_Highlight')
@@ -131,14 +111,12 @@ function removeHighlight(id) {
     document.getElementById(id).classList.remove('box_Highlight')
 }
 function paperBinNote(position) {
-    // console.log(position)
     let num;
     for (let i = 0; i < allTasksArray.length; i++) {
         if (allTasksArray[i].id == position) {
             num = i;
         }
     }
-    // console.log(num)
     let index = "trash";
     allTasksArray[num]['board'] = index;
     pushToServer();
@@ -151,7 +129,6 @@ function deleteNote(position) {
         }
     }
     pushToServer();
-    // downloadFromServer();
     updateHTML();
 }
 async function pushToServer() {
